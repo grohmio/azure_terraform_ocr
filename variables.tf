3c78aa91fd5327e782ce7732dffa6cfbe468f36f
Andreas Grohmann <andreas.grohmann@hotmail.de>
2022-11-10 14:46:42 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "location" {
   description = "azure region where the resources will be created"
   default     = "westeurope"
 }
+
+variable "COGNITIVE_SERVICE_ACCOUNT_NAME" {
+  type        = string
+  description = "azure cognitive service account name, needs to be unique because of use in url"
+}
+
+variable "OCR_STORAGE_ACCOUNT_NAME" {
+  type        = string
+  description = "azure storage account name, , needs to be unique because of use in url"
+}
