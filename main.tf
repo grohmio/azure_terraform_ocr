9e8c77cbf95a0658a1b3c44c70bc83d9981193d8
Andreas Grohmann <ag17@gmx.de>
2022-11-10 13:14:46 +0100

    add variable storage account name

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "azure-terraform-ocr-tfstate"
-    storage_account_name = "tfstatestoracc"
+    storage_account_name = var.storage_account_name
     container_name       = "tfstatestorcon"
     key                  = "terraform.tfstate"
   }
